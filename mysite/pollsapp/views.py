f24e708b0ecab57354f2661391ec66c416de7563
Anandhu Vijayan <38665597+Anandhuvijayan@users.noreply.github.com>
2021-03-19 09:59:45 +0100

    Update views.py

--- mysite/pollsapp/views.py
+++ mysite/pollsapp/views.py
@@ -25,18 +25,17 @@ def detail(request, question_id):
     return render(request, 'pollsapp/detail.html', {'question': question})
 
 def results(request, question_id):
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
+    
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'pollsapp/results.html', {'question': question})
 
 def vote(request, question_id):  
      question = get_object_or_404(Question, pk=question_id)
-    # return HttpResponse("You're voting on question %s." % question_id)    
+   
      try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
      except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
+        
         return render(request, 'pollsapp/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -44,7 +43,5 @@ def vote(request, question_id):
      else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+       
         return HttpResponseRedirect(reverse('pollsapp:results', args=(question.id,)))
